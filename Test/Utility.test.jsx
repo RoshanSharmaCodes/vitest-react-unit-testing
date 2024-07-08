6113e8d2c4cb68c22c8cedff69f5a582e326f66f
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2024-07-08 20:28:03 +0530

    * Test for State Management.

--- Test/Utility.test.jsx
+++ Test/Utility.test.jsx
@@ -1,28 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe, expect, it, test } from 'vitest'
 import CompoOne from "../src/Component/CompoOne";
 import App from "../src/App"
 
 
-describe("HomePage Testing",()=>{
-    test("Checking For Heading",()=>{
-      render(<App/>)
-      const title = screen.getByText("Vite + React")
-      expect(title).toBeInTheDocument()
-    })
+describe("HomePage Testing", () => {
+  test("Checking For Heading", () => {
+    render(<App />)
+    const title = screen.getByText("Vite + React")
+    expect(title).toBeInTheDocument()
+  })
 })
 
 
 describe('Component Testing -> CompoOne', () => {
-  test("Checking For Title",()=>{
-    render(<CompoOne title="Diwali Offer"/>)
+  test("Checking For Title", () => {
+    render(<CompoOne title="Diwali Offer" />)
     const title = screen.getByText("Diwali Offer")
     expect(title).toBeInTheDocument()
   })
-  test("Checking For  Empty Title",()=>{
+  test("Checking For  Empty Title", () => {
     render(<CompoOne />)
     const title = screen.getByText("Promo Offer")
     expect(title).toBeInTheDocument()
+  })
+  test("Checking for Counter", () => {
+    render(<CompoOne />)
+    let counterVal = screen.getByTestId('count');
+    expect(counterVal).toHaveTextContent('Counter0');
+
+    const incBtn = screen.getByText("Inc")
+    const decBtn = screen.getByText("Dec")
+
+    fireEvent.click(incBtn)
+    counterVal = screen.getByTestId('count')
+    expect(counterVal).toHaveTextContent('Counter1')
+
+    fireEvent.click(decBtn)
+    fireEvent.click(decBtn)
+    counterVal = screen.getByTestId('count')
+    expect(counterVal).toHaveTextContent('Counter-1')
+
   })
 })
 
--- src/Component/CompoOne.jsx
+++ src/Component/CompoOne.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CompoOne({title="Promo Offer"}) {
+  const [counter, setCounter] = useState(0)
   return (
     <>
     <div>{title}</div>
+    <div data-testid="count"><h1>Counter</h1>{counter}</div>
+    <button onClick={()=> setCounter(counter + 1)}>Inc</button>
+    <button onClick={()=> setCounter(counter - 1)}>Dec</button>
     <input type="text" id="eventName" />
     </>
   )
